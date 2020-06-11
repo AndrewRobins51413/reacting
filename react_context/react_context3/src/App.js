dcc0191d1aad884f51380595f3d7addf14cf98c1
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-06-11 12:49:50 -0700

    react context exercise

---
+++ react_context/react_context3/src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import Header from "./Header"
+import {UserContextConsumer} from "./userContext"
+
+import './App.css';
+
+
+class App extends Component {
+  state = {
+    newUserName:"Andy321"
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState ({[name]: value})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <UserContextConsumer>
+            {username => (
+              <p className="main">No new notes, {username} !</p>
+            )}
+          </UserContextConsumer>
+        </main>
+        <input
+          type="text"
+          name="username"
+          placeholder="New Username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button> Change Username</button>
+      </div>
+    )
+  }
+}
+
+export default App;
