8f02466f02935131bac78b52882364c08f8e3626
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-06-17 11:39:52 -0700

    react hooks useState simple app

---
+++ react_context/hooksusestate/src/App.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [inputData, setInputData] = useState({firstName:"", lastName:""})
+  const[contactsData, setContactsData] = useState([])
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setInputData(prevInputData => ({...prevInputData, [name]:value}))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+      setContactsData( prevContactData => [...prevContactData, inputData])
+  }
+
+  const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>
+      {contact.firstName} {contact.lastName}
+    </h2>)
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="First Name"
+          name="firstName"
+          value={inputData.firstName}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Last Name"
+          name="lastName"
+          value={inputData.lastName}
+          onChange={handleChange}
+        />
+        <br />
+        <button> Add Contact </button>
+      </form>
+      {contacts}
+    </>
+  )
+}
+
+export default App;
